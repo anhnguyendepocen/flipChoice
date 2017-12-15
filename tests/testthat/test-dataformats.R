9ec5cce90e9518a59c05d491bfbd3253111cf978
JustinCCYap <justinyapcc@gmail.com>
2017-12-15 11:16:21 +1100

    Use InterceptWarnings

--- tests/testthat/test-dataformats.R
+++ tests/testthat/test-dataformats.R
@@ -1,7 +1,5 @@
 context("Data formats")
 
-library(readxl)
-
 findInstDirFile <- function(file)
 {
     file.path(system.file("testdata", package = "flipChoice", mustWork = TRUE),
@@ -47,10 +45,11 @@ test_that("jmp format", {
 })
 
 test_that("jmp labels format", {
-    result <- FitChoiceModel(design.file = jmp.levels.design.file,
+    ExpectWarning(result <- FitChoiceModel(design.file = jmp.levels.design.file,
                              choices = choices.jmp.levels,
                              questions = tasks.jmp.levels,
                              hb.iterations = 10, hb.chains = 1,
-                             hb.warnings = FALSE)
+                             hb.warnings = FALSE),
+        "9 respondents with missing data were omitted from the analysis.")
     expect_error(print(result), NA)
 })
