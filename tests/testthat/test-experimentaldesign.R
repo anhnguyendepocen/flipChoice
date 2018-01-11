bb63354bba5124a26c935266e04f02ba42374b86
jakehoare <jake_hoare@hotmail.com>
2018-01-11 16:50:22 +1100

    DS-1743 add level balance outputs

--- tests/testthat/test-experimentaldesign.R
+++ tests/testthat/test-experimentaldesign.R
@@ -8,33 +8,24 @@ attribute.levels <- list(c("yamaha", "honda", "ducati", "triumph", "bmw", "kawas
                          c("red", "green", "blue", "yellow", "black", "white", "silver"))
 prohibitions <- matrix(c("ducati", "125cc", "manual", "red", "ducati", "", "manual", "red", "honda", "All", "", "yellow"),
                        ncol = 4, byrow = TRUE)
-n.questions <- 100
+n.questions <- 30
 alternatives.per.question <- 3
 
 # Automated test case
 experiment <- CreateExperiment(c(3, 5, 7, 10), 20)
 
-# TODO convert below to loop
-
-test_that("Random", {
-    expect_error(ChoiceModelDesign("Random", attribute.levels, n.questions,
-                                   alternatives.per.question, prohibitions), NA)
-    expect_error(ChoiceModelDesign("Random", experiment$attribute.levels, n.questions,
-                                   alternatives.per.question, experiment$prohibitions), NA)
-})
-
-test_that("Shortcut", {
-    expect_error(ChoiceModelDesign("Shortcut", attribute.levels, n.questions,
-                                   alternatives.per.question, prohibitions), NA)
-    expect_error(ChoiceModelDesign("Shortcut", experiment$attribute.levels, n.questions,
-                                   alternatives.per.question, experiment$prohibitions), NA)
-})
-
-test_that("Enumerated", {
-    expect_error(ChoiceModelDesign("Complete enumeration", attribute.levels, n.questions,
-                                   alternatives.per.question, prohibitions), NA)
-    expect_error(ChoiceModelDesign("Complete enumeration", experiment$attribute.levels, n.questions,
-                                   alternatives.per.question, experiment$prohibitions), NA)
-})
+# TODO convert below to loop and test the different outputs
 
 
+for (model in c("Random", "Shortcut", "Complete enumeration")) {
+    for (output in c("Attributes and levels", "Prohibitions", "Unlabelled design", "Labelled design",
+                     "Level balances")) {
+
+        test_that(paste(model, output), {
+            expect_error(ChoiceModelDesign(model, attribute.levels, n.questions,
+                                           alternatives.per.question, prohibitions, output), NA)
+            expect_error(ChoiceModelDesign(model, experiment$attribute.levels, n.questions,
+                                           alternatives.per.question, experiment$prohibitions, output), NA)
+        })
+    }
+}
