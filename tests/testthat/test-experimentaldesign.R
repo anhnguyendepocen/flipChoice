2042ff2a07b5fef57b4fe42209986b1881fe0c89
jakehoare <jake_hoare@hotmail.com>
2018-01-19 14:28:44 +1100

    DS-1743 Imporve costs for complete enumeration

--- tests/testthat/test-experimentaldesign.R
+++ tests/testthat/test-experimentaldesign.R
@@ -15,10 +15,11 @@ alternatives.per.question <- 3
 # Automated test case
 experiment <- CreateExperiment(c(3, 5, 7, 10), 20)
 
+# TODO add tests for equality with specific designs
 
 for (model in c("Random", "Shortcut", "Complete enumeration")) {
     for (output in c("Attributes and levels", "Prohibitions", "Unlabelled design", "Labelled design",
-                     "Balances and overlaps")) {
+                     "Balances and overlaps", "Standard errors")) {
 
         test_that(paste(model, output), {
             expect_error(print(ChoiceModelDesign(model, experiment$attribute.levels, n.questions, n.versions,
