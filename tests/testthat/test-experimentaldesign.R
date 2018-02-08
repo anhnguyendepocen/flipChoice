d8f0728e1216d21c623b0755178e2f8ce4b7d73c
jakehoare <jake_hoare@hotmail.com>
2018-02-08 17:21:14 +1100

    DS-1743 fix Random design to avoid duplicates per question

--- tests/testthat/test-experimentaldesign.R
+++ tests/testthat/test-experimentaldesign.R
@@ -18,7 +18,7 @@ experiment <- CreateExperiment(c(3, 5, 7, 10), 20)
 # TODO add tests for equality with specific designs
 tfile <- tempfile()
 withr::with_output_sink(tfile, {
-    for (model in c("Random", "Shortcut")) {  # , "Complete enumeration"
+    for (model in c("Random", "Shortcut", "Complete enumeration")) {
         for (output in c("Attributes and levels", "Prohibitions", "Unlabeled design", "Labeled design",
                          "Balances and overlaps", "Standard errors")) {
 
