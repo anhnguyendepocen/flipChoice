6b244d01614245edbbb3144cae9a4798ba787690
jakehoare <jake_hoare@hotmail.com>
2018-02-13 14:49:21 +1100

    DS-1743 separate out Balanced Overlap, add labeled alternatives to shortcut

--- tests/testthat/test-experimentaldesign.R
+++ tests/testthat/test-experimentaldesign.R
@@ -1,66 +1,49 @@
 context("Experimental design")
 
 # Manual test case
-attribute.levels <- list(c("yamaha", "honda", "ducati", "triumph", "bmw", "kawasaki"),
+manual.attribute.levels <- list(c("yamaha", "honda", "ducati", "triumph", "bmw", "kawasaki"),
                          c("125cc", "250cc", "500cc"),
                          c("manual", "automatic"),
                          c("red", "green", "blue", "yellow", "black", "white", "silver"))
-names(attribute.levels) <- c("brand", "engine", "transmission", "colour")
-prohibitions <- matrix(c("ducati", "125cc", "manual", "red", "ducati", "", "manual", "red", "honda", "All", "", "yellow"),
+names(manual.attribute.levels) <- c("brand", "engine", "transmission", "colour")
+manual.prohibitions <- matrix(c("ducati", "125cc", "manual", "red", "ducati", "", "manual", "red", "honda", "All", "", "yellow"),
                        ncol = 4, byrow = TRUE)
-n.questions <- 10
-n.versions <- 2
-alternatives.per.question <- 3
 
 # Automated test cases
 experiment <- CreateExperiment(c(3, 5, 7, 10), 20)
 experiment2 <- CreateExperiment(c(4, 4, 4, 4), 0)
 
+levels.test.cases <- list(manual.attribute.levels, experiment$attribute.levels, experiment2$attribute.levels)
+prohibitions.test.cases <- list(manual.prohibitions, experiment$prohibitions, experiment2$prohibitions)
+questions.test.cases <- c(10, 12, 6)
+versions.test.cases <- c(2, 1, 6)
+alternatives.test.cases <- c(3, 3, 6)
+none.test.cases <- c(0, 2, 0)
+labeled.test.cases <- c(FALSE, TRUE, FALSE)
 
 # TODO add tests for equality with specific designs
 tfile <- tempfile()
 withr::with_output_sink(tfile, {
-    for (model in c("Random", "Complete enumeration" )) {  # can't handle prohibitions with "Shortcut"
+    for (model in c("Random", "Complete enumeration", "Balanced overlap", "Shortcut")) {  # can't handle prohibitions with "Shortcut"
         for (output in c("Attributes and levels", "Prohibitions", "Unlabeled design", "Labeled design",
                          "Balances and overlaps", "Standard errors")) {
-
-                test_that(paste(model, output), {
+            for (i in seq(length(levels.test.cases))) {
+                test_that(paste(model, output, "Test case", i), {
+                    prohibitions <- if(model == "Shortcut") NULL else prohibitions.test.cases[[i]]
                     cmd <- ChoiceModelDesign(design.algorithm = model,
-                                             attribute.levels = experiment$attribute.levels,
-                                             prior = NULL,
-                                             n.questions = 10,
-                                             n.versions = 2,
-                                             alternatives.per.question = 3,
-                                             prohibitions = experiment$prohibitions,
-                                             none.alternatives = 0,
-                                             labeled.alternatives = FALSE,
-                                             output = output)
-                    expect_error(print(cmd), NA)
-                    cmd <- ChoiceModelDesign(design.algorithm = model,
-                                             attribute.levels = attribute.levels,
-                                             prior = NULL,
-                                             n.questions = 12,
-                                             n.versions = 2,
-                                             alternatives.per.question = 3,
-                                             prohibitions = prohibitions,
-                                             none.alternatives = 2,
-                                             labeled.alternatives = TRUE,
-                                             output = output)
-                    expect_error(print(cmd), NA)
-                    cmd <- ChoiceModelDesign(design.algorithm = model,
-                                             attribute.levels = experiment2$attribute.levels,
-                                             prior = NULL,
-                                             n.questions = 6,
-                                             n.versions = 6,
-                                             alternatives.per.question = 5,
-                                             prohibitions = experiment2$prohibitions,
-                                             none.alternatives = 0,
-                                             labeled.alternatives = FALSE,
-                                             output = output)
+                                                 attribute.levels = levels.test.cases[[i]],
+                                                 prior = NULL,
+                                                 n.questions = questions.test.cases[i],
+                                                 n.versions = versions.test.cases[i],
+                                                 alternatives.per.question = alternatives.test.cases[i],
+                                                 prohibitions = prohibitions,
+                                                 none.alternatives = none.test.cases[i],
+                                                 labeled.alternatives = labeled.test.cases[i],
+                                                 output = output)
                     expect_error(print(cmd), NA)
                 })
-
-      }
-  }
+            }
+        }
+    }
 })
 unlink(tfile)
